b967cafb8396d07a7215b69cf42608cc02450ba4
Xavier Guichet <contact@xavier-guichet.fr>
2024-12-21 07:45:47 +0100

    Chores : move default dishes to db

--- app/src/main/java/com/example/mealplangenerator/data/repository/MainDishesRepository.kt
+++ app/src/main/java/com/example/mealplangenerator/data/repository/MainDishesRepository.kt
@@ -1,72 +1,9 @@
 package com.example.mealplangenerator.data.repository
 
-import com.example.mealplangenerator.enums.Duration
 import com.example.mealplangenerator.data.model.MainDish
 import com.example.mealplangenerator.data.model.MealCriteria
 import com.example.mealplangenerator.enums.MealTime
 import com.example.mealplangenerator.room.AppDatabase
-
-private val mainDishes: List<MainDish>
-    get() = mutableListOf<MainDish>(
-        MainDish("Cordon bleu", MealTime.LUNCH, Duration.QUICK, 2),
-        MainDish("Pavé de saumon", MealTime.LUNCH, Duration.QUICK),
-        MainDish("Cotes de porc citron", MealTime.LUNCH, Duration.SHORT),
-        MainDish("Steak haché végétaux", MealTime.LUNCH, Duration.QUICK, 1, arrayOf("Pois chiches", "Lentilles", "Pois Cassés")),
-
-        MainDish("Salade Ceasar", MealTime.ANY, Duration.SHORT),
-        MainDish("Dinde cacahuète / petit légumes", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Filet de dinde aux 3 poivrons", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Bagel saumon citron vert", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Nuggets poulets courgette,", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Steack dinde aux poivrons", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Croquette de hadock", MealTime.ANY, Duration.MEDIUM),
-
-        MainDish("Pates", MealTime.ANY, Duration.QUICK),
-
-        MainDish("Couscous", MealTime.ANY, Duration.MEDIUM, 1, arrayOf("Boeuf", "Falafel")),
-        MainDish("Paella",MealTime.ANY, Duration.MEDIUM),
-        MainDish("Riz a l'espagnole", MealTime.LUNCH, Duration.SHORT),
-
-        MainDish("Curry poulet", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Ragout de patate", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Oeufs champignon", MealTime.ANY, Duration.QUICK),
-        MainDish("Omelette ratatouille", MealTime.ANY, Duration.QUICK),
-        MainDish("Tarte", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Burger", MealTime.ANY, Duration.LONG),
-        MainDish("Burger végétarien", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Galette", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Chili con carne", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Quinoa enchiladas", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Boeuf Korean", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Crevette courgette", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Rouleau de printemps", MealTime.ANY, Duration.MEDIUM),
-        MainDish("Tacos", MealTime.ANY, Duration.MEDIUM),
-
-        MainDish("Poulet [du monde]", MealTime.ANY, Duration.MEDIUM, 1, arrayOf("tikka massala",  "miel et au vinaigre", "tandoori")),
-        MainDish("Roulés de poulet au jambon cru", MealTime.ANY, Duration.LONG),
-        MainDish("Poulet bacon BBQ", MealTime.ANY, Duration.LONG),
-        MainDish("Croque monsieur", MealTime.ANY, Duration.QUICK),
-
-        // Dinner
-        MainDish("Boeuf mariné au cinq épices", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Fish n Chips", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Ravioli", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Raclette", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Tartiflette", MealTime.DINNER, Duration.LONG),
-        MainDish("Soupe", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Fondue de luxe", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Croissant aux fromages", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Sushi", MealTime.DINNER, Duration.MEDIUM),
-        MainDish("Mac N Cheese", MealTime.DINNER, Duration.SHORT),
-        MainDish("Pates", MealTime.DINNER, Duration.MEDIUM, 3, arrayOf("Bolognèse maison", "Bolognèse Végé", "JFK Pasta", "Carbonara", "Aux courgettes")),
-        MainDish("Pizza", MealTime.DINNER, Duration.MEDIUM, 1, arrayOf("Chorizo", "Royale", "Bacon", "Tartiflette")),
-        MainDish("Risotto", MealTime.DINNER, Duration.MEDIUM, 1, arrayOf("aux champignons", "aux légumes", "au chorizo")),
-        MainDish("Lasagne", MealTime.DINNER, Duration.MEDIUM),
-        // Dinner + Long
-        MainDish("Hachis parmentier", MealTime.DINNER, Duration.LONG),
-        MainDish("Curry de crevette", MealTime.DINNER, Duration.LONG),
-        MainDish("Boeuf bourguignon", MealTime.DINNER, Duration.LONG),
-    )
 
 class MainDishesRepository(private val db: AppDatabase) {
     private var allDishes = mutableListOf<MainDish>()
--- app/src/main/java/com/example/mealplangenerator/room/AppDatabase.kt
+++ app/src/main/java/com/example/mealplangenerator/room/AppDatabase.kt
@@ -15,17 +15,99 @@ import kotlinx.coroutines.launch
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dishDao(): DishDao?
 
-    private class AppDatabaseCallback(private val scope: CoroutineScope): Callback() {
+    private class AppDatabaseCallback(private val scope: CoroutineScope) : Callback() {
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
             INSTANCE?.let { database ->
                 scope.launch {
                     val dishDao = database.dishDao()
-                    dishDao?.deleteAll()
+                    if (dishDao == null)
+                        return@launch
 
-                    dishDao?.insert(Dish("Cordon bleu", MealTime.LUNCH, Duration.QUICK, 2))
-                    dishDao?.insert(Dish("Pavé de saumon", MealTime.LUNCH, Duration.QUICK))
-                    dishDao?.insert(Dish("Cotes de porc citron", MealTime.LUNCH, Duration.SHORT))
+                    dishDao.deleteAll()
+
+                    dishDao.insertAll(
+                        Dish("Cordon bleu", MealTime.LUNCH, Duration.QUICK, 2),
+                        Dish("Pavé de saumon", MealTime.LUNCH, Duration.QUICK),
+                        Dish("Cotes de porc citron", MealTime.LUNCH, Duration.SHORT),
+                        Dish("Salade Caesar", MealTime.ANY, Duration.SHORT),
+                        Dish("Dinde cacahuète / petit légumes", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Filet de dinde aux 3 poivrons", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Bagel saumon citron vert", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Nuggets poulet courgette,", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Steak dinde aux poivrons", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Croquette de haddock", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Pates", MealTime.ANY, Duration.QUICK),
+                        Dish("Paella", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Riz a l'espagnole", MealTime.LUNCH, Duration.SHORT),
+                        Dish("Curry poulet", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Ragout de patate", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Oeufs champignon", MealTime.ANY, Duration.QUICK),
+                        Dish("Omelette ratatouille", MealTime.ANY, Duration.QUICK),
+                        Dish("Tarte", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Burger", MealTime.ANY, Duration.LONG),
+                        Dish("Burger végétarien", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Galette", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Chili con carne", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Quinoa enchiladas", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Boeuf Korean", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Crevette courgette", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Rouleau de printemps", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Tacos", MealTime.ANY, Duration.MEDIUM),
+                        Dish("Roulés de poulet au jambon cru", MealTime.ANY, Duration.LONG),
+                        Dish("Poulet bacon BBQ", MealTime.ANY, Duration.LONG),
+                        Dish("Croque-monsieur", MealTime.ANY, Duration.QUICK),
+                        Dish("Boeuf mariné au 5 épices", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Fish n Chips", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Ravioli", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Raclette", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Tartiflette", MealTime.DINNER, Duration.LONG),
+                        Dish("Soupe", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Fondue de luxe", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Croissant aux fromages", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Sushi", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Mac N Cheese", MealTime.DINNER, Duration.SHORT),
+                        Dish("Lasagne", MealTime.DINNER, Duration.MEDIUM),
+                        Dish("Hachis parmentier", MealTime.DINNER, Duration.LONG),
+                        Dish("Curry de crevette", MealTime.DINNER, Duration.LONG),
+                        Dish("Boeuf bourguignon", MealTime.DINNER, Duration.LONG),
+                        Dish(
+                            "Steak haché végétaux",
+                            MealTime.LUNCH,
+                            Duration.QUICK,
+                            1
+                        ), // , arrayOf("Pois chiches", "Lentilles", "Pois Cassés")),
+                        Dish(
+                            "Couscous",
+                            MealTime.ANY,
+                            Duration.MEDIUM,
+                            1
+                        ), // , arrayOf("Boeuf", "Falafel")),
+                        Dish(
+                            "Poulet [du monde]",
+                            MealTime.ANY,
+                            Duration.MEDIUM,
+                            1
+                        ), // , arrayOf("tikka massala",  "miel et au vinaigre", "tandoori", "saté")),
+                        Dish(
+                            "Pates",
+                            MealTime.DINNER,
+                            Duration.MEDIUM,
+                            3
+                        ), // , arrayOf("Bolognèse maison", "Bolognèse Végé", "JFK Pasta", "Carbonara", "Aux courgettes")),
+                        Dish(
+                            "Pizza",
+                            MealTime.DINNER,
+                            Duration.MEDIUM,
+                            1
+                        ), // , arrayOf("Chorizo", "Royale", "Bacon", "Tartiflette")),
+                        Dish(
+                            "Risotto",
+                            MealTime.DINNER,
+                            Duration.MEDIUM,
+                            1
+                        ), // , arrayOf("aux champignons", "aux légumes", "au chorizo")),
+                    )
                 }
             }
         }
@@ -43,9 +125,10 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "mpg_db"
-                ).addCallback(AppDatabaseCallback(scope))
-                .allowMainThreadQueries()
-                .build()
+                )
+                    .addCallback(AppDatabaseCallback(scope))
+                    .allowMainThreadQueries()
+                    .build()
                 INSTANCE = instance
                 // return instance
                 instance
