55e9753637c803ca8f6e7ffd8dc50b1126cc696b
Xavier Guichet <contact@xavier-guichet.fr>
2025-01-10 07:07:25 +0100

    Tests : remove test for legacyWmpFormat

--- app/src/test/java/com/example/mealplangenerator/data/model/WeeklyMealPlanTest.kt
+++ app/src/test/java/com/example/mealplangenerator/data/model/WeeklyMealPlanTest.kt
@@ -32,20 +32,4 @@ class WeeklyMealPlanTest {
         assertTrue(wmp.containsKey(MealSlot(DayOfWeek.MONDAY, MealTime.LUNCH)))
         assertTrue(wmp.containsKey(MealSlot(DayOfWeek.SUNDAY, MealTime.DINNER)))
     }
-
-    @Test
-    fun getLegacyWmpFormat()
-    {
-        wmp[MealSlot(DayOfWeek.MONDAY, MealTime.LUNCH)] = Meal(MainDish("My dish 1"))
-        wmp[MealSlot(DayOfWeek.WEDNESDAY, MealTime.DINNER)] = Meal(MainDish("My dish 2"))
-
-        val legacyWMPMealPlan = wmp.mealPlan
-
-        assertTrue(legacyWMPMealPlan.containsKey(DayOfWeek.MONDAY))
-        assertTrue(legacyWMPMealPlan[DayOfWeek.MONDAY]?.containsKey(MealTime.LUNCH) ?: false)
-        assertEquals("My dish 1", legacyWMPMealPlan[DayOfWeek.MONDAY]?.get(MealTime.LUNCH)?.name ?: "Name Not Found")
-        assertTrue(legacyWMPMealPlan.contains(DayOfWeek.WEDNESDAY))
-        assertTrue(legacyWMPMealPlan[DayOfWeek.WEDNESDAY]?.containsKey(MealTime.DINNER) ?: false)
-        assertEquals("My dish 2", legacyWMPMealPlan[DayOfWeek.WEDNESDAY]?.get(MealTime.DINNER)?.name ?: "Name Not Found")
-    }
 }
